006d37edeebdbdee5687643099099dc0ea672d91
n-guitar <nguitar001@gmail.com>
2021-04-03 15:32:38 +0900

    mod: Channel Model > add create_at field & mod name field unique option

--- webchat/models.py
+++ webchat/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Channel(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     topic = models.TextField(max_length=255)
     is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
