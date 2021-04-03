ac1d36621adb849b01694ce0ca90b3dcac16f701
n-guitar <nguitar001@gmail.com>
2021-04-03 15:35:48 +0900

    Add: Message models

--- webchat/admin.py
+++ webchat/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Channel
+from .models import Channel, Message
 
 
 admin.site.register(Channel)
+admin.site.register(Message)
--- webchat/models.py
+++ webchat/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Channel(models.Model):
@@ -9,3 +10,14 @@ class Channel(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    cnannel_id = models.ForeignKey(
+        Channel, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return ("id:{}, cnannel_id:{}, user_id:{}".format(self.id, self.cnannel_id, self.user_id))
